d8e920347f73cd13c2aa9e35e94ff98dad02ab8c
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-02-15 02:15:42 -0200

    criacao do repositorio de command para pedido com entity

---
+++ PedidoCompra/PedidoCompra/Repositorios/PedidoCommandRepository.cs
@@ -0,0 +1,28 @@
+﻿using PedidoCompra.Domain.PedidoAggregate;
+using PedidoCompra.Contextos;
+using PedidoCompra.Domain.PedidoAggregate.Interfaces.Repositorios;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PedidoCompra.Repositorios
+{
+    public class PedidoCommandRepository : IPedidoCommandRepository
+    {
+        private readonly Contexto _contexto;
+        public PedidoCommandRepository(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<Pedido> AdicionarAsync(Pedido pedido)
+        {
+            return (await _contexto.AddAsync(pedido)).Entity;
+        }
+
+        public async Task<IEnumerable<Pedido>> ListarAsync()
+        {
+            return await _contexto.Pedidos.ToArrayAsync();
+        }
+    }
+}
