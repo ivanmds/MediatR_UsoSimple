bc85308430b1673381f961145e26d4c19f74af2e
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-02-15 02:16:26 -0200

    criacao do commando de adicionar pedido

---
+++ PedidoCompra/PedidoCompra/Domain/PedidoAggregate/Validations/PedidoAddValidation.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using PedidoCompra.Domain.PedidoAggregate.Commands;
+
+namespace PedidoCompra.Domain.PedidoAggregate.Validations
+{
+    public class PedidoAddValidation : PedidoValidation<PedidoAddCommand>
+    {
+        public PedidoAddValidation()
+        {
+            ValidarId();
+            ValidarCriado();
+            ValidarStatus();
+            ValidarDescricao();
+        }
+    }
+}
