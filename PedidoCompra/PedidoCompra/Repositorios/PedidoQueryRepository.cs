9691d1da77383aa7ebd9b4dec14ed0be2f8efc09
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-02-15 02:15:17 -0200

    criação do repositorio pedido para leitura com dapper

---
+++ PedidoCompra/PedidoCompra/Repositorios/PedidoQueryRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PedidoCompra.Domain.PedidoAggregate;
+using PedidoCompra.Domain.PedidoAggregate.Interfaces.Repositorios;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace PedidoCompra.Repositorios
+{
+    public class PedidoQueryRepository : IPedidoQueryRepository, IDisposable
+    {
+        private readonly SqlConnection _db;
+        public PedidoQueryRepository()
+        {
+            _db = new SqlConnection(@"Data Source=localhost\SQLServer;Initial Catalog=PedidoCompra;Persist Security Info=True;User ID=sa;Password=123456");
+        }
+
+        public async Task<IEnumerable<Pedido>> ListarAsync()
+        {
+            return await _db.QueryAsync<Pedido>("SELECT Id, Criado, Descricao, Status FROM Pedido");
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
