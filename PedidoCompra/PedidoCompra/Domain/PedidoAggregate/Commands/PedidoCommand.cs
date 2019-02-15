0c326cd980c1d853c29a0ce82acbf7d40ff8aa9f
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-02-15 02:17:41 -0200

    criacao do pedido comando base para comandos do pedido

---
+++ PedidoCompra/PedidoCompra/Domain/PedidoAggregate/Commands/PedidoCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PedidoCompra.Domain.PedidoAggregate.Commands
+{
+    public abstract class PedidoCommand : Command
+    {
+        public Guid Id { get; protected set; }
+        public DateTime Criado { get; protected set; }
+        public string Descricao { get; protected set; }
+        public PedidoStatus Status { get; protected set; }
+    }
+}
