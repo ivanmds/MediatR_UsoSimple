8c0d9c88b004b38d6c06666c44f738292546f14e
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-02-15 02:16:05 -0200

    utilizando FluetValidation para criar regra gerais do pedido

---
+++ PedidoCompra/PedidoCompra/Domain/PedidoAggregate/Validations/PedidoValidation.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using PedidoCompra.Domain.PedidoAggregate.Commands;
+using System;
+
+namespace PedidoCompra.Domain.PedidoAggregate.Validations
+{
+    public abstract class PedidoValidation<T> : AbstractValidator<T> where T : PedidoCommand
+    {
+        protected void ValidarId()
+        {
+            RuleFor(p => p.Id)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id informado é inválido.");
+        }
+
+        protected void ValidarStatus()
+        {
+            RuleFor(p => p.Status)
+                .NotEqual(PedidoStatus.Definir)
+                .WithMessage("Status deve ser definido");
+        }
+
+        protected void ValidarDescricao()
+        {
+            RuleFor(p => p.Descricao)
+                    .MinimumLength(5)
+                    .MaximumLength(200)
+                    .WithMessage("Descricao inválido. Descrição deve conter de 5 a 200 caracteres.");
+
+        }
+
+        protected void ValidarCriado()
+        {
+            RuleFor(p => p.Criado)
+                .NotEqual(DateTime.MinValue)
+                .WithMessage("Informe um data válida.");
+        }
+    }
+}
