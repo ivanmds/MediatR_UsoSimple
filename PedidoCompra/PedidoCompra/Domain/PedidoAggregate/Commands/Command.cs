99c5796c9e1f8d7fbcbd31a8ebd478c2bc88844e
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-02-15 02:18:04 -0200

    criacao do command base para todos tipos de comandos

---
+++ PedidoCompra/PedidoCompra/Domain/PedidoAggregate/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using System;
+
+namespace PedidoCompra.Domain.PedidoAggregate.Commands
+{
+    public abstract class Command : IRequest<ValidationResult>
+    {
+        public DateTime Data { get; protected set; }
+        public Guid AggregateId { get; protected set; }
+        public TipoMensagem Tipo { get; protected set; }
+        public ValidationResult Validacao { get; protected set; } = new ValidationResult();
+        public abstract bool EhValido();
+    }
+}
