4c298df84ccb0edbbcdcf24e03ff13f1d045d8e5
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-02-15 02:16:55 -0200

    interface para pedido repositorio leitura

---
+++ PedidoCompra/PedidoCompra/Domain/PedidoAggregate/Interfaces/Repositorios/IPedidoQueryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PedidoCompra.Domain.PedidoAggregate.Interfaces.Repositorios
+{
+    public interface IPedidoQueryRepository
+    {
+        Task<IEnumerable<Pedido>> ListarAsync();
+    }
+}
