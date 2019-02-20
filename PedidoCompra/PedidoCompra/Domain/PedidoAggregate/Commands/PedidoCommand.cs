8fa06959def602c1dd56a0b32e50fd7adbe7ba38
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-02-20 00:09:36 -0300

    ajuste no modelo do command

--- PedidoCompra/PedidoCompra/Domain/PedidoAggregate/Commands/PedidoCommand.cs
+++ PedidoCompra/PedidoCompra/Domain/PedidoAggregate/Commands/PedidoCommand.cs
@@ -5,18 +5,25 @@ namespace PedidoCompra.Domain.PedidoAggregate.Commands
 {
     public abstract class PedidoCommand : Command
     {
-        public Guid Id { get; protected set; }
-        public DateTime Criado { get; protected set; }
-        public string Descricao { get; protected set; }
-        public PedidoStatus Status { get; protected set; }
+        public Guid Id { get; set; }
+        public DateTime Criado { get; set; }
+        public string Descricao { get; set; }
+        public PedidoStatus Status { get; set; }
 
-        public List<PedidoItemDto> Itens { get; protected set; }
+        public List<PedidoItemDto> Itens { get; set; }
     }
 
     public class PedidoItemDto
     {
+        public Guid Id { get; set; }
         public string Descricao { get; set; }
         public float Quantidade { get; set; }
         public decimal ValorUnitario { get; set; }
+
+        public PedidoItemDto()
+        {
+            if (Id.Equals(Guid.Empty))
+                Id = Guid.NewGuid();
+        }
     }
 }
