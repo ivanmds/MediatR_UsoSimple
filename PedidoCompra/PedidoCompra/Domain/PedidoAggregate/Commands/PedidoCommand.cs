da6fc8bd6e819fb085407725910fdc16d3b8cbda
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-02-20 00:13:42 -0300

    correção do command

--- PedidoCompra/PedidoCompra/Domain/PedidoAggregate/Commands/PedidoCommand.cs
+++ PedidoCompra/PedidoCompra/Domain/PedidoAggregate/Commands/PedidoCommand.cs
@@ -5,12 +5,11 @@ namespace PedidoCompra.Domain.PedidoAggregate.Commands
 {
     public abstract class PedidoCommand : Command
     {
-        public Guid Id { get; set; }
-        public DateTime Criado { get; set; }
-        public string Descricao { get; set; }
-        public PedidoStatus Status { get; set; }
-
-        public List<PedidoItemDto> Itens { get; set; }
+        public Guid Id { get; protected set; }
+        public DateTime Criado { get; protected set; }
+        public string Descricao { get; protected set; }
+        public PedidoStatus Status { get; protected set; }
+        public List<PedidoItemDto> Itens { get; protected set; }
     }
 
     public class PedidoItemDto
