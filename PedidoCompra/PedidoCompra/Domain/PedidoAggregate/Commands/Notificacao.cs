4b40376b4b123ee5ed4074fbc21306994ff16776
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-02-28 02:32:03 -0300

    ajuste no comando e notificacao

--- PedidoCompra/PedidoCompra/Domain/PedidoAggregate/Commands/Notificacao.cs
+++ PedidoCompra/PedidoCompra/Domain/PedidoAggregate/Commands/Notificacao.cs
@@ -1,12 +1,8 @@
 ﻿using MediatR;
-using System;
 
 namespace PedidoCompra.Domain.PedidoAggregate.Commands
 {
     public class Notificacao : INotification
     {
-        public Guid Id { get; set; }
-        public DateTime Criado { get; set; }
-        public string Descricao { get; set; }
     }
 }
--- PedidoCompra/PedidoCompra/Domain/PedidoAggregate/Commands/Pedido/Add/PedidoAddNotification.cs
+++ PedidoCompra/PedidoCompra/Domain/PedidoAggregate/Commands/Pedido/Add/PedidoAddNotification.cs
@@ -1,6 +1,11 @@
-﻿namespace PedidoCompra.Domain.PedidoAggregate.Commands.Pedido.Add
+﻿using System;
+
+namespace PedidoCompra.Domain.PedidoAggregate.Commands.Pedido.Add
 {
     public class PedidoAddNotification : Notificacao
     {
+        public Guid Id { get; set; }
+        public DateTime Criado { get; set; }
+        public string Descricao { get; set; }
     }
 }
